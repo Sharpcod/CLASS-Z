6bee3fb0dca6a829871ee2a1e660108104122d32
Sharpcod <95564134+Sharpcod@users.noreply.github.com>
2023-03-26 02:39:34 +0530

    Add files via upload

---
+++ obj_trk_final.py
@@ -0,0 +1,46 @@
+import cv2
+
+xs = []
+ys = []
+
+video = cv2.VideoCapture("footvolleyball.mp4")
+#load tracker 
+tracker = cv2.TrackerCSRT_create()
+
+#read the first frame of the video
+success,img = video.read()
+
+#selct the bounding box on the image
+bbox = cv2.selectROI("tracking",img,False)
+
+#initialise the tracker on the img and the bounding box
+tracker.init(img,bbox)
+
+
+def drawBox(img,bbox):
+    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+
+
+while True:
+    check,img = video.read()   
+    success,bbox = tracker.update(img)
+
+    if success:
+        drawBox(img,bbox)
+    else:
+        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
+    #Activity 3
+    #call the function to track the goal
+   
+    cv2.imshow("result",img)
+            
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        print("Closing")
+        break
+
+video.release()
+cv2.destroyALLwindows() 
